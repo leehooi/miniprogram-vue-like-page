7004f24916609f5cc97c706b24ed9db8dc6935d2
Lee.Li <lee.li@wendywuchina.com>
2019-02-14 11:31:01 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const mixin = require('./mixin');
-const computed = require('./computed');
-const watch = require('./watch');
 
 module.exports = function (page) {
 
     mixin.enable(page,
-        [computed, watch]
-            .concat(getApp().mixins || [])
+        [
+            require('./computed'),
+            require('./watch')
+        ].concat(getApp().mixins || [])
             .concat(page.mixins || []));
 
     return page;
